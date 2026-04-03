4d2d8d4e7aaf35ce780565cad82ddb9182c7406e
Sarahwillock <111887339+Sarahwillock@users.noreply.github.com>
2026-04-03 12:24:43 -0300

    Update June.tsx

--- src/pages/June.tsx
+++ src/pages/June.tsx
@@ -84,7 +84,7 @@ export default function June() {
           <div className="md:col-span-2 relative h-72 border-4 border-move-blue group overflow-hidden neo-brutalist-shadow">
             <div className="absolute inset-0 bg-neutral-900">
               <img 
-                src="https://lh3.googleusercontent.com/aida-public/AB6AXuCSPdN2pB06pRpNjvO3jJbIKi8_mOKL0n1pEVnENGYgtGdzP9OJb0lDYQo2NX7-GuXnbcacWnszeyatvNOTdtfaTykuw6XLTB98Ppp3-dsYZAgZuHFgLlW-iw1lxkkkLWGYvmPuDNgc7GlsksnB-qzy9wmu4Z8sCUrHSKqQJXska9spWvQmUBcdnCn0ChMyYKdDBaeORVwxzD8A1O3_h5sP5yRXU3eZbWAR9KV6mCTJaU62G63uOBOTyh8yF9J_wHeDmlir1LnED7rI" 
+                src="/images/june.manu.jpg" 
                 alt="Map"
                 className="absolute inset-0 w-full h-full object-cover opacity-40 grayscale group-hover:grayscale-0 transition-all duration-700"
                 referrerPolicy="no-referrer"
